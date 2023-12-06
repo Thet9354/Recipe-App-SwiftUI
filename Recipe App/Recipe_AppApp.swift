583cc8c63b8bc317edea66a3401c7dfabca5566b
PhoonThetPine <thetpine254@gmail.com>
2023-12-06 14:15:51 +0800

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Phoon Thet Pine on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
